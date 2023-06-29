5e1b9704c88f0b32c9678c7365fc12c4cf80de65
TANISHQ SATSANGI <tanishqsatsangi@gmail.com>
2023-06-29 10:14:39 +0530

    Dev handled UI to display, and DB Sqlite
    added UI and SQllite

---
+++ app/src/main/java/com/project/radiusagentassignment/DatabaseHelper.kt
@@ -0,0 +1,4 @@
+package com.project.radiusagentassignment
+
+class DatabaseHelper {
+}
--- app/src/main/java/com/project/radiusagentassignment/fields/OptionField.kt
+++
@@ -1,7 +0,0 @@
-package com.project.radiusagentassignment.fields
-
-import com.project.radiusagentassignment.fields.BaseField
-
-class OptionField : BaseField() {
-    var parentField: String? = null
-}
